ed980caaa854dfa5cf12e1c3cfc0b4d452db2386
remiangelo <justryeoi@gmail.com>
2024-08-08 09:32:35 +0700

    search box appears and disappears

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import logo from "../imgs/logo.png";
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
+
+    const [ searchBoxVisibility, setSearchBoxVisibility ] = useState(false);
+
+
     return (
         <nav className="navbar">
 
@@ -9,7 +14,7 @@ const Navbar = () => {
                 <img src={logo} className="w-full" />
             </Link>
 
-            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+            <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto " + ( searchBoxVisibility ? "show" : "hide" )}>
                 <input 
                     type="text"
                     placeholder="Search"
@@ -20,7 +25,7 @@ const Navbar = () => {
 
             <div className="flex items-center gap-3 md:gap-6 ml:auto">
                 <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center ">
-                    <i className="fi fi-rr-search text-2xl"></i>
+                    <i className="fi fi-rr-search text-xl" onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}></i>
                 </button>
             </div>
 
